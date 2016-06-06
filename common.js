73cd0bfdc973b4d9947e60d6a3d4070de5f34ea3
Adam Webb <adam.webb@lightsource.ca>
2016-06-06 15:38:37 -0600

    use 32-bit math and add NaN filter on result

--- common.js
+++ common.js
@@ -5,13 +5,14 @@ importClass(Packages.ij.plugin.ImageCalculator);
 // basic background subtraction
 function bkg_subtract(imp1, imp2, imp3) {
 	var ic = new ImageCalculator();
-	var imp4 = ic.run("Subtract create", imp3, imp1);
+	var imp4 = ic.run("Subtract create 32-bit", imp3, imp1);
 	//imp4.show();
 	imp4.setTitle("image_d");
-	var imp5 = ic.run("Subtract create", imp2, imp1);
+	var imp5 = ic.run("Subtract create 32-bit", imp2, imp1);
 	//imp5.show();
 	imp5.setTitle("flat_d");
 	var imp6 = ic.run("Divide create 32-bit", imp4, imp5);
+	IJ.run(imp6, "Remove NaNs...", "radius=2");
 	imp6.setTitle("image_norm");
 	return imp6;
 }
